d65ed4132afe86a80eff4e2590123fbb01c4e7cd
gheo43dn <ngvatuan01052002@gmail.com>
2025-05-22 22:31:55 +0700

    test prettier

--- services/property-service/src/index.js
+++ services/property-service/src/index.js
@@ -1,15 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
 const propertyRoutes = require('./routes/property.routes');
-
 app.use(express.json());
 app.use('/properties', propertyRoutes);
-
-const PORT = process.env.PORT || 4002;
-console.log(process.env.PORT);
-
-app.listen(PORT, () => {
-  console.log(`Property service is running on port ${PORT}`);
-});
