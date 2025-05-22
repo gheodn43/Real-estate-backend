3f467d2a12d68a8929f938c21a0c9b3ad53fd184
gheo43dn <ngvatuan01052002@gmail.com>
2025-05-22 22:21:49 +0700

    chore: update project configuration and Docker setup
    - Add .eslintignore and .prettierignore files to exclude unnecessary files from linting and formatting
    - Update .prettierrc with new formatting rules
    - Modify Dockerfile for property-service to optimize build process
    - Update docker-compose files to include properties_db
    - Add linting and formatting scripts to package.json
    - Update property-service .gitignore to remove .env

--- services/property-service/src/index.js
+++ services/property-service/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
 const propertyRoutes = require('./routes/property.routes');
 
@@ -7,6 +8,8 @@ app.use(express.json());
 app.use('/properties', propertyRoutes);
 
 const PORT = process.env.PORT || 4002;
+console.log(process.env.PORT);
+
 app.listen(PORT, () => {
   console.log(`Property service is running on port ${PORT}`);
 });
