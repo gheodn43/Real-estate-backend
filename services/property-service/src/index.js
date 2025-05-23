229b4e00091b6471f4f2203ff0d3445a53ce8359
gheo43dn <ngvatuan01052002@gmail.com>
2025-05-23 14:54:12 +0700

    update property service

--- services/property-service/src/index.js
+++ services/property-service/src/index.js
@@ -1,6 +1,31 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 const propertyRoutes = require('./routes/property.routes');
+
+dotenv.config();
+const app = express();
 app.use(express.json());
-app.use('/properties', propertyRoutes);
+app.use(
+  cors({
+    origin: [
+      'https://app.propintel.id.vn',
+      'http://gateway:4000',
+      'http://auth-service:4001',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  })
+);
+
+app.use('/prop/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/prop', propertyRoutes);
+const PORT = process.env.PORT || 4002;
+app.listen(PORT);
