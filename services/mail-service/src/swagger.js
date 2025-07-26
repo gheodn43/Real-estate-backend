b63a39707154b6a8f5ea6ea47211e0df0e2cb303
gheo43dn <ngvatuan01052002@gmail.com>
2025-07-26 09:05:38 +0700

    update

--- services/mail-service/src/swagger.js
+++ services/mail-service/src/swagger.js
@@ -11,7 +11,7 @@ const options = {
     servers: [
       {
         url: 'https://api.propintel.id.vn',
-        description: 'Property server',
+        description: 'Mail server',
       },
     ],
   },
