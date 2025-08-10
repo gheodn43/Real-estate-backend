24c8db64be15a2533004ea3f2973cea91acf2161
gheo43dn <ngvatuan01052002@gmail.com>
2025-08-10 23:15:50 +0700

    upate

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -9,25 +9,19 @@ const app = express();
 
 app.get('/swagger.json', async (req, res) => {
   try {
-    const [
-      auth, 
-      property, 
-      agentReview, 
-      blog, 
-      //agentChat
-    ] = await Promise.all([
+    const [auth, property, agentReview, blog, agentChat] = await Promise.all([
       axios.get('http://auth-service:4001/swagger.json'),
       axios.get('http://property-service:4002/swagger.json'),
       axios.get('http://agent-review-service:4004/swagger.json'),
       axios.get('http://blog-service:4005/swagger.json'),
-      //axios.get('http://agent-chat-service:3000/swagger.json'),
+      axios.get('http://agent-chat-service:3000/swagger.json'),
     ]);
     const mergedSpec = mergeSpecs([
       auth.data,
       property.data,
       agentReview.data,
       blog.data,
-      //agentChat.data,
+      agentChat.data,
     ]);
 
     mergedSpec.info = {
