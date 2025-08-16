8e3afc5467d7f2d32d8f7d84f36a08aca21b9a7e
KietNguyen2301 <Kietde160233@gmail.com>
2025-08-17 00:12:48 +0700

    add get list property2

--- services/auth-service/src/helpers/propertyClient.js
+++ services/auth-service/src/helpers/propertyClient.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
+
 const getListPropOfUser = async (userId, page, limit, type, search, needsType, token) => {
-
-
     try {
         const res = await axios.get(
             `http://property-service:4002/prop/get-list-property-of-user/${userId}`,
@@ -16,14 +15,9 @@ const getListPropOfUser = async (userId, page, limit, type, search, needsType, t
                 },
             }
         );
-        console.log(res.data.data);
         return res.data.data;
     } catch (err) {
-        console.log(err.message);
-
-        
     }
-
 }
 
 module.exports = {
